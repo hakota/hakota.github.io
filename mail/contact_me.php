79151c86d6ec7fc1a74e588aa420cbb033da7288
hakota <araki.kenta.db@gmail.com>
2017-07-08 18:21:30 +0900

    Fix

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,12 @@
+
 <?php
+
+include '../ChromePhp.php';
+ChromePhp::log('############');
+ChromePhp::log($_POST['name']);
+ChromePhp::log($_POST['email']);
+ChromePhp::log($_POST['message']);
+
 // Check for empty fields
 if(empty($_POST['name']) ||
    empty($_POST['email']) ||
@@ -13,11 +21,14 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'araki.kenta.db@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = '';
+$email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@hakota.net\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+// $headers = "";
+// $headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body);
+
+// mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
